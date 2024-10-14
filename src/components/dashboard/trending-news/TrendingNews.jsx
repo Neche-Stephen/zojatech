78b3046e3576ab56260bfe996889017400856228
Chinecherem <chinecheremubawike@gmail.com>
2024-10-15 00:32:30 +0100

    Minor updates

--- src/components/dashboard/trending-news/TrendingNews.jsx
+++ src/components/dashboard/trending-news/TrendingNews.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 import ICONS from "../../../assets/dashboard/";
 
 export default function TrendingNews() {
-
-    const news = [
-        { value: "$4,000", title: "Russia & Ukraine War", icon: ICONS.FACEBOOK },
-        { value: "$2,120", title: "Video Monetization", icon: ICONS.FACEBOOK },
-        { value: "$1,752", title: "Community Buildup", icon: ICONS.FACEBOOK },
-      ]
+  const news = [
+    { value: "$4,000", title: "Russia & Ukraine War", icon: ICONS.UKRAINE_NEWS},
+    { value: "$2,120", title: "Video Monetization", icon: ICONS.ELON_MUSK_NEWS },
+    { value: "$1,752", title: "Community Buildup", icon: ICONS.FUEL_PRICE_NEWS},
+  ];
   return (
     <div className="w-[93.2%] mt-8 mb-4 mx-auto md:mx-0 p-5 bg-white rounded-2xl">
       <div className="text-[#3a3a45] text-xl font-bold font-['Lexend'] leading-relaxed">
@@ -17,8 +16,15 @@ export default function TrendingNews() {
       <div className="flex flex-col gap-3">
         {news.map((item, index) => {
           return (
-            <div key={index} className=" bg-white p-4 rounded-xl flex justify-between items-center gap-[65px] shadow border border-[#f1f1f1] ">
-              <div className=" w-2/12">
+            <div
+              key={index}
+              className=" bg-white p-4 rounded-xl flex justify-between items-center gap-[65px] shadow border border-[#f1f1f1] "
+            >
+              <div className="[@media(min-width:640px)_and_(max-width:1108px)]:hidden">
+                <img className="w-full" src={item.icon} alt="" />
+              </div>
+
+              <div>
                 <div className="text-[#3a3a45] text-[25px] font-bold font-['Lexend'] leading-[33.25px] mb-1">
                   {item.value}
                 </div>
@@ -26,14 +32,10 @@ export default function TrendingNews() {
                   {item.title}
                 </div>
               </div>
-
-              <div className="[@media(min-width:640px)_and_(max-width:1108px)]:hidden">
-                <img className="w-full" src={item.icon} alt="" />
-              </div>
             </div>
           );
         })}
       </div>
     </div>
-  )
+  );
 }
