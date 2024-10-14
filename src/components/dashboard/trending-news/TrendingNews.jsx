1aa2b399df23b842b0a053560f3627ad4ef8fd75
Chinecherem <chinecheremubawike@gmail.com>
2024-10-15 00:40:34 +0100

    Worked on trending news responsiveness

--- src/components/dashboard/trending-news/TrendingNews.jsx
+++ src/components/dashboard/trending-news/TrendingNews.jsx
@@ -3,9 +3,21 @@ import ICONS from "../../../assets/dashboard/";
 
 export default function TrendingNews() {
   const news = [
-    {title: "Russia & Ukraine War", subtitle: "Marketing is evolving. It's chang...", icon: ICONS.UKRAINE_NEWS},
-    { title: "Elon Musk bought Twitter", subtitle: "Twitter is the most useful social pl...", icon: ICONS.ELON_MUSK_NEWS },
-    { title: "Fuel Crisis Everywhere", subtitle: "Due to covid situation in 2020 the...", icon: ICONS.FUEL_PRICE_NEWS},
+    {
+      title: "Russia & Ukraine War",
+      subtitle: "Marketing is evolving. It's chang...",
+      icon: ICONS.UKRAINE_NEWS,
+    },
+    {
+      title: "Elon Musk bought Twitter",
+      subtitle: "Twitter is the most useful social pl...",
+      icon: ICONS.ELON_MUSK_NEWS,
+    },
+    {
+      title: "Fuel Crisis Everywhere",
+      subtitle: "Due to covid situation in 2020 the...",
+      icon: ICONS.FUEL_PRICE_NEWS,
+    },
   ];
   return (
     <div className="w-[93.2%] mt-8 mb-4 mx-auto md:mx-0 p-5 bg-white rounded-2xl">
@@ -18,15 +30,19 @@ export default function TrendingNews() {
           return (
             <div
               key={index}
-              className=" bg-white p-4 rounded-xl flex justify-between items-center gap-2 shadow border border-[#f1f1f1] "
+              className=" bg-white p-4 rounded-xl flex items-center gap-2 shadow border border-[#f1f1f1] "
             >
-              <div className="[@media(min-width:640px)_and_(max-width:1108px)]:hidden">
+              <div className="">
                 <img className="w-full" src={item.icon} alt="" />
               </div>
 
               <div>
-              <div className="text-[#3a3a45] text-sm font-semibold font-['Lexend']">{item.title}</div>
-              <div className="text-[#808086] text-xs font-light font-['Lexend'] leading-none">{item.subtitle}</div>
+                <div className="text-[#3a3a45] text-sm font-semibold font-['Lexend']">
+                  {item.title}
+                </div>
+                <div className="text-[#808086] text-xs font-light font-['Lexend'] leading-none">
+                  {item.subtitle}
+                </div>
               </div>
             </div>
           );
